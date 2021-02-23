1fee3ce4a3bcc3cb688efdc9fc2ca4cdb236f268
7FFF00 <t42please@gmail.com>
2021-02-22 16:41:53 -0800

    ADD slight optimization with early loop break

--- leetcode_dailies/longestNiceSubstring.js
+++ leetcode_dailies/longestNiceSubstring.js
@@ -1,4 +1,4 @@
-// Longest Nice Substring, leetcode
+// Longest Nice Substring, https://leetcode.com/problems/longest-nice-substring
 // Runtime: 612 ms, faster than 100.00% of JavaScript online submissions for Longest Nice Substring.
 // Memory Usage: 55.3 MB, less than 100.00% of JavaScript online submissions for Longest Nice Substring.
 
@@ -14,31 +14,35 @@ const niceWordChecker = (word) => {
 	return true;
 };
 
-var longestNiceSubstring = function(s) {
+const longestNiceSubstring = function(s) {
 	let longestNiceWord = '';
+
 	for (let i = 0; i < s.length; i += 1) {
 		let currentWord = s.substr(i);
+		if (currentWord.length < longestNiceWord) {
+			break;
+		}
 		while (!niceWordChecker(currentWord)) {
 			currentWord = currentWord.substr(0, currentWord.length - 1);
 		}
 		longestNiceWord = currentWord.length > longestNiceWord.length ? currentWord : longestNiceWord;
 	}
+
 	return longestNiceWord;   
 };
-
-// console.log(niceWordChecker('abc'));
-// console.log(niceWordChecker('abAB'));
-// console.log(niceWordChecker('aAa'));
-// console.log(niceWordChecker(''));
-console.log(longestNiceSubstring('abDdeCbDeagHi'));
 
 /*
 	input: string
 	output: string (first largest found word)
 	constraints: none
 	edge cases: string is empty, funky arrangement of matching cases, ie: 'yAbBcCeDdbbbbaE'
+
 	find longest nice substring, where nice is words containing alt case counterparts of every letter
 	possible ideas:
 		'abDdeCbDeagHi'
 		go over every letter and try backtracking at end until it reaches a nice word, and see if longer than current found word
+
+	possible optimizations:
+		cut out early through iterations if future words to be searched through are shorter than longest word found so far
+		find each pair and perform a binary search in either direction from each of them as long as they've never been explored before
 */
