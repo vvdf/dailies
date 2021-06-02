fb99a904af5ee1ed6729ff37f2df0820ed9fbca4
vvdf <t42please@gmail.com>
2021-06-02 16:29:05 -0700

    Complete rust implementation, with stats

--- rust_weeklies/shortest_palindrome/src/main.rs
+++ rust_weeklies/shortest_palindrome/src/main.rs
@@ -2,16 +2,32 @@ struct Solution {}
 
 impl Solution {
     pub fn shortest_palindrome(s: String) -> String {
-        if Solution::palindrome_validity_check(s) {
-            String::from("true")
+        let mut word = String::from(&s);
+        let idx = Solution::find_internal_palindrome(&s);
+        println!("{}", idx);
+        for c in String::from(&s[idx..]).chars() {
+            word = String::from(c.to_string()) + &word[..];
+        }
+        word
+    }
+
+    pub fn find_internal_palindrome (s: &String) -> usize {
+        let mut longest_length: usize = 0;
+        for (i, _c) in s.chars().enumerate() {
+            if Solution::palindrome_check(String::from(&s[..i + 1])) {
+                longest_length = i;
+            }
+        }
+
+        if s.len() < 1 {
+            longest_length
         } else {
-            String::from("false")
+            longest_length + 1
         }
     }
 
-    pub fn palindrome_validity_check(s: String) -> bool {
+    pub fn palindrome_check(s: String) -> bool {
         for (c, rc) in s.chars().zip(s.chars().rev()) {
-            // let c = s.chars().nth(i).unwrap();
             if c != rc {
                 return false;
             }
@@ -23,12 +39,14 @@ impl Solution {
 fn main() {
     println!("{}", Solution::shortest_palindrome(String::from("example")));
     println!("{}", Solution::shortest_palindrome(String::from("wawaw")));
+    println!("{}", Solution::shortest_palindrome(String::from("ursesrun")));
+    println!("{}", Solution::shortest_palindrome(String::from("")));
 }
 
 /*  
- *  Leetcode:
- *  Runtime:
- *  Memory Usage:
+ *  Leetcode: https://leetcode.com/problems/shortest-palindrome
+ *  Runtime: 1344ms, faster than 20.00% of Rust online submissions
+ *  Memory Usage: 2.6MB, less than 20.00% of Rust online submissions
  *
  *  Goal: given string 's', convert 's' to a palindrome by adding chars
  *      in front, return shortest palindrome you can find
