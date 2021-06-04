cb03fdbae44b1e68f8303fe257abd74f24cd7c00
vvdf <t42please@gmail.com>
2021-06-04 12:22:14 -0700

    Add rust quicksort attempt init

---
+++ rust_weeklies/quicksort/src/main.rs
@@ -0,0 +1,35 @@
+struct Solution {}
+
+impl Solution {
+    pub fn sort_array(nums: Vec<i32>) -> Vec<i32> {
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+ *  Leetcode: https://leetcode.com/problems/sort-an-array/
+ *  Runtime:
+ *  Memory:
+ *
+ *  Goal: sort an array/vector of integers in ascending order
+ *
+ *  Input: vector of integers
+ *  Output: vector of integers that have been sorted
+ *  Constraints: none
+ *  Edge Cases: none that I'm aware of yet
+ *
+ *  Approach: In particular I'm planning to use this as practice for
+ *      implementing quicksort using Rust, as such it will be my approach
+ *
+ *      Quicksort rough algorithm:
+ *          nominate a 'pivot' point to serve as center to current step
+ *          of sorting, assigning categories of less-than or greater-than
+ *          to all remaining entries
+ *
+ *          recursively repeat for sub collections
+ *
+ *          return combined collections
+ */
