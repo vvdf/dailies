cc84035368f19680505101a18ec91c20160e1460
Victor Diez-Fugitt <t42please@gmail.com>
2022-01-21 18:53:52 -0800

    Add notes and plan for longst valid parens

--- rust_weeklies/longest_valid_parenthesis/src/main.rs
+++ rust_weeklies/longest_valid_parenthesis/src/main.rs
@@ -6,6 +6,33 @@ impl Solution {
     }
 }
 
+fn main() {
+
+}
+
 /*
+ * https://leetcode.com/problems/longest-valid-parentheses/
  *
+ * input: array containing ONLY '()' characters
+ * output: integer
+ * constraints: 
+ * edge cases: 
+ *  - no valid parenthesis present
+ *  - entire string is a valid parenthesis
+ *
+ * potential approaches:
+ *  - iterate through each character and expanding out as long as
+ *      left bracket count is greater than or equal to right bracket
+ *      count
+ *      - no valid parenthesis collection will start with anything
+ *          other than a left bracket '(' or end with ')'
+ *          (potential optimization)
+ *      - big O of n^2
+ *  - iterate through the list to count total number of left and right
+ *      brackets, iterate once more to create an additional array listing
+ *      a disparity count of each side, then reducing the string based on
+ *      where it's weighted incorrectly?
+ *      - could work to just trim the edges of the string until the
+ *          left and right bracket count is balanced
+ *          if this works, big O of n
  * /
