5a1151377225249c59198051d30e6f360d3bf4d4
Victor Diez-Fugitt <t42please@gmail.com>
2022-03-22 22:28:32 -0700

    Add rust solution start to valid parentheses

---
+++ 2022/rust/template_main.rs
@@ -0,0 +1,16 @@
+struct Solution {}
+
+impl Solution {
+    pub fn test_function(s: String) -> bool {
+    }
+}
+
+fn main() {
+    let test_input = "example";
+    println!("{:?}", Solution);
+}
+
+/*
+ *
+ *
+ */
---
+++ 2022/rust/valid_parentheses/src/main.rs
@@ -0,0 +1,19 @@
+struct Solution {}
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let bracket_stack = Vec<char>;
+    }
+}
+
+fn main() {
+    let test_input = "()";
+    let test_in2 = "()[]{}";
+    let test_in3 = "(]";
+    println!("{:?}", Solution);
+}
+
+/*
+ *
+ *
+ */
