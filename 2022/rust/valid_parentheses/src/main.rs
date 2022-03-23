da5721b47e7cfb126822fb88c93d4473a4d12712
Victor Diez-Fugitt <t42please@gmail.com>
2022-03-22 22:53:14 -0700

    Partial rust impl. solution for valid parens

--- 2022/rust/valid_parentheses/src/main.rs
+++ 2022/rust/valid_parentheses/src/main.rs
@@ -3,14 +3,24 @@ struct Solution {}
 impl Solution {
     pub fn is_valid(s: String) -> bool {
         let bracket_stack: Vec<char> = Vec::new();
+        let mut bracket_key = std::collections::HashMap::new();
+        bracket_key.insert(String::from("{"), String::from("}"));
+        bracket_key.insert(String::from("["), String::from("]"));
+        bracket_key.insert(String::from("("), String::from(")"));
+
+        for i in &s[..] {
+            
+        }
     }
+
+    bracket_stack.len() < 1
 }
 
 fn main() {
     let test_input = "()";
     let test_in2 = "()[]{}";
     let test_in3 = "(]";
-    println!("{:?}", Solution);
+    println!("{:?}", Solution::is_valid(test_input));
 }
 
 /*
